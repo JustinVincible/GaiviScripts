403a4222e20ae0f6fbd3c4b2b90c0f79633bf3d4
Justin <90917308+JustinVincible@users.noreply.github.com>
2025-04-02 12:54:45 -0400

    Update train_job.sh

--- train_job.sh
+++ train_job.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --time=4-0                 # Max job runtime (4 days)
-#SBATCH -p SISLab                  # Partition name
+#SBATCH --time=1-0                 # Max job runtime~:
+#SBATCH -p general                  # Partition name
 #SBATCH --nodes=1                   # Number of nodes
-#SBATCH -w "GPU47"                  # Specific node
-#SBATCH --job-name=APPLE            # Job name
+#SBATCH -w "GPU2"                  # Specific node
+#SBATCH --job-name=<name>            # Job name
 #SBATCH --ntasks-per-node=1         # Number of tasks per node
 #SBATCH --gpus-per-node=1           # GPUs per node
 #SBATCH --output=train_job.out           # Output log file
 #SBATCH --mail-type=BEGIN,END,FAIL  # Email notifications
-#SBATCH --mail-user=jmcmillen@usf.edu # Email address for SLURM notifications
+#SBATCH --mail-user=<name>@usf.edu # Email address for SLURM notifications
 
 # Set up distributed training variables
 export MASTER_PORT=$((10000 + SLURM_JOB_ID % 10000))
@@ -19,10 +19,10 @@ echo "Launching Python script..."
 
 # Activate conda environment
 source ~/.bashrc
-conda activate f_env || { echo "Failed to activate Conda environment"; exit 1; }
+conda activate <env_name> || { echo "Failed to activate Conda environment"; exit 1; }
 
 # Print environment variables for debugging
 printenv | sort > train_job.out
 
 # Run Python script with logging
-python -u  train_apple.py --cfg "/nvme/SISLab/Justin/f/configs/a.yaml" > train_job.out
+python -u  train_apple.py > train_job.out
